6335b583401f4fc8d9646486e1e0997b23fc0157
peiyuchao <bladeralien@126.com>
2017-01-19 14:30:43 +0800

    initial commit

---
+++ CarND-Alexnet-Feature-Extraction/train_feature_extraction.py
@@ -0,0 +1,38 @@
+import pickle
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from alexnet import AlexNet
+
+# TODO: Load traffic signs data.
+training_file = "./traffic-signs-data/train.p"
+testing_file = "./traffic-signs-data/test.p"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+    
+X_train, y_train = train['features'], train['labels']
+X_test, y_test = test['features'], test['labels']
+
+from sklearn.utils import shuffle
+X_train, y_train = shuffle(X_train, y_train)
+
+# TODO: Split data into training and validation sets.
+
+# TODO: Define placeholders and resize operation.
+
+# TODO: pass placeholder as first argument to `AlexNet`.
+fc7 = AlexNet(..., feature_extract=True)
+# NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
+# past this point, keeping the weights before and up to `fc7` frozen.
+# This also makes training faster, less work to do!
+fc7 = tf.stop_gradient(fc7)
+
+# TODO: Add the final layer for traffic sign classification.
+
+# TODO: Define loss, training, accuracy operations.
+# HINT: Look back at your traffic signs project solution, you may
+# be able to reuse some the code.
+
+# TODO: Train and evaluate the feature extraction model.
